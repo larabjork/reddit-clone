68607f3e34b602a1f788d650a9d3b29af4d439d5
Student <example@example.com>
2020-02-18 16:40:43 -0800

    add image uploader

--- src/components/NewPostForm.jsx
+++ src/components/NewPostForm.jsx
@@ -14,13 +14,28 @@ function NewPostForm(props){
   }
 
   const formStyle = {
+    paddingTop: '50px',
     textAlign: 'center',
-    border: '2px solid black',
-    backgroundColor: "lightblue",
-    width: '300px'
-
+    height: '500px'
   }
+  const uploadedImage = React.useRef(null);
+  const imageUploader = React.useRef(null);
+
+  const handleImageUpload = e => {
+     const [file] = e.target.files;
+     if (file) {
+       const reader = new FileReader();
+       const {current} = uploadedImage;
+       current.file = file;
+       reader.onload = (e) => {
+           current.src = e.target.result;
+       }
+       reader.readAsDataURL(file);
+     }
+   };
+
   return (
+    <div>
     <div>
       <form style={formStyle} onSubmit={handleNewPostFormSubmission}>
         <input
@@ -36,7 +51,45 @@ function NewPostForm(props){
           ref={(textarea) => {_body = textarea;}}/>
           <br></br>
         <button type='submit'>Post it!</button>
-      </form>
+      <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center"
+      }}
+    >
+      <input
+        type="file"
+        accept="image/*"
+        onChange={handleImageUpload}
+        ref={imageUploader}
+        style={{
+          display: "none"
+        }}
+      />
+      <div
+        style={{
+          height: "60px",
+          width: "60px",
+          border: "1px dashed black"
+        }}
+        onClick={() => imageUploader.current.click()}
+      >
+        <img
+          ref={uploadedImage}
+          style={{
+            width: "20%",
+            height: "20%",
+            position: "absolute"
+          }}
+        />
+      </div>
+      Click to upload Image
+
+    </div>
+    </form>
+    </div>
     </div>
   );
 }
